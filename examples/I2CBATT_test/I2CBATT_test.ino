4c1d24f29cb330f199811cab912fd81fec064784
Roman <romansklyar15@gmail.com>
2022-08-08 21:33:53 +0300

    Update I2CBATT_test.ino

--- examples/I2CBATT_test/I2CBATT_test.ino
+++ examples/I2CBATT_test/I2CBATT_test.ino
@@ -26,8 +26,6 @@
  * Проект HDQBATT на GitHub - https://github.com/S-LABc/HDQBATT
  * 
  * Контакты:
- ** YouTube - https://www.youtube.com/channel/UCbkE52YKRphgkvQtdwzQbZQ
- ** Telegram - https://www.t.me/slabyt
  ** GitHub - https://github.com/S-LABc
  ** Gmail - romansklyar15@gmail.com
  * 
